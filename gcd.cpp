659ec3a6a7ecd7cd8625ef555be02dfae3b19961
German Yakimov <german13yakimov@gmail.com>
2018-06-01 17:14:22 +0700

    GCD Algorithm

---
+++ gcd.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+  int rm;
+  while (b != 0)
+  {
+    rm = a % b;
+    a = b;
+    b = rm;
+  }
+
+  return a;
+}
+
+int main()
+{
+  int a, b;
+  cout<<"Enter two numbers"<<endl;
+  cin>>a>>b;
+  cout<<gcd(a, b);
+
+  return 0;
+}
