816029307ca66a911024a6bd36b6789ae6268c3c
German Yakimov <german13yakimov@gmail.com>
2018-06-03 11:22:26 +0700

    added new methods fpr doubly linked list

--- linked_lists/doubly_list.cpp
+++ linked_lists/doubly_list.cpp
@@ -61,7 +61,7 @@ public:
   void print()
   {
     element* tmp_element = top_limiter->next;
-    
+
     while (tmp_element != lower_limiter)
     {
       cout<<tmp_element->data<<endl;
@@ -69,61 +69,141 @@ public:
     }
   }
 
-  // int get_index_by_data(int target) //this method returns element by data
-  // {
-  //
-  // }
-  //
-  // int* get_data_by_index(int index)
-  // {
-  //
-  // }
-  //
-  // void add(int data)
-  // {
-  //
-  // }
-  //
-  // void delete_element_by_index(int index)
-  // {
-  //   if (index < 0 || index >= size())
-  //     return;
-  //
-  // }
-  //
-  // void delete_element_by_data(int target)
-  // {
-  //
-  // }
-  //
-  // void clear()
-  // {
-  //
-  // }
-  //
-  // void delete_all()
-  // {
-  //
-  // }
-  //
-  // void input()
-  // {
-  //
-  // }
-  //
-  // void change_data_by_index(int index, int new_val)
-  // {
-  //   if (index < 0 || index >= size())
-  //     return;
-  // }
+  int get_index_by_data(int target) //this method returns element by data
+  {
+    int index = 0;
+    element* tmp_element = top_limiter->next;
+
+    while (tmp_element != lower_limiter)
+    {
+      if (tmp_element->data == target)
+        return index;
+
+      tmp_element = tmp_element->next;
+      index++;
+    }
+
+    return -1;
+  }
+
+  int* get_data_by_index(int index)
+  {
+    if (index < 0 || index >= size())
+      return NULL;
+
+    element* tmp_element = top_limiter->next;
+
+    while(index > 0)
+    {
+      tmp_element = tmp_element->next;
+      index--;
+    }
+
+    return &tmp_element->data;
+  }
+
+  void add(int data)
+  {
+    element* old_head = top_limiter->next;
+
+    element* new_element = new element;
+    new_element->data = data;
+    new_element->next = old_head;
+    new_element->prev = top_limiter;
+
+    old_head->prev = new_element;
+    top_limiter->next = new_element;
+
+  }
+
+  void delete_element_by_index(int index)
+  {
+    if (index < 0 || index >= size())
+      return;
+
+    element* tmp_element = top_limiter->next;
+    element* next;
+    element* prev;
+
+    while(index > 0)
+    {
+      tmp_element = tmp_element->next;
+      index--;
+    }
+
+    prev = tmp_element->prev;
+    next = tmp_element->next;
+
+    prev->next = next;
+    next->prev = prev;
+    delete tmp_element;
+  }
+
+  void delete_element_by_data(int target)
+  {
+    delete_element_by_index(get_index_by_data(target));
+  }
+
+  void clear()
+  {
+    element* tmp_element = top_limiter->next;
+    element* next = tmp_element->next;
+
+    while (tmp_element != lower_limiter)
+    {
+      delete tmp_element;
+      tmp_element = next;
+      next = next->next;
+    }
+
+    top_limiter->next = lower_limiter;
+    lower_limiter->prev = top_limiter;
+  }
+
+  void delete_all()
+  {
+    clear();
+    delete top_limiter;
+    delete lower_limiter;
+  }
+
+  void input()
+  {
+    element* tmp_element = top_limiter->next;
+    cout<<"Enter all items in the list separated by enter/space: "<<endl;
+
+    while (tmp_element != lower_limiter)
+    {
+      cin>>tmp_element->data;
+      tmp_element = tmp_element->next;
+    }
+  }
+
+  void change_data_by_index(int index, int new_val)
+  {
+    if (index < 0 || index >= size())
+      return;
+
+    element* tmp_element = top_limiter->next;
+
+    while (index)
+    {
+      tmp_element = tmp_element->next;
+      index--;
+    }
+
+    tmp_element->data = new_val;
+  }
 
 };
 
 int main()
 {
   srand(time(0));
-  list my_list(5);
+  list my_list(10);
+  my_list.input();
   my_list.print();
 
+  my_list.delete_all();
   return 0;
 }
