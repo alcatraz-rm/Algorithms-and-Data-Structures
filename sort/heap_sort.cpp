0ce78aad31a5845d74a19e8f91fae22f56f84d42
German Yakimov <german13yakimov@gmail.com>
2018-11-11 19:03:56 +0700

    Implement heap sort.

--- sort/heap_sort.cpp
+++ sort/heap_sort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <ctime>
 using namespace std;
 
 vector<int> make_heap(vector<int> arr)
@@ -10,7 +11,6 @@ vector<int> make_heap(vector<int> arr)
     for (i = 0; i < size; i++)
     {
         index = i;
-
 
         while (index != 0)
         {
@@ -33,17 +33,77 @@ vector<int> make_heap(vector<int> arr)
 
 vector<int> remove_top_item(vector<int> heap)
 {
-    heap.erase(heap.begin());
-    return make_heap(heap);
+    heap[0] = *(heap.end() - 1);
+    heap.pop_back();
+
+    int index, child_1, child_2, size = heap.size(), swap_child, j;
+    index = 0;
+
+    while (true)
+    {
+        if (index >= size)
+        {
+            break;
+        }
+
+        child_1 = index * 2 + 1;
+        child_2 = child_1 + 1;
+
+        if (child_1 >= size)
+        {
+            child_1 = index;
+        }
+
+        if (child_2 >= size)
+        {
+            child_2 = index;
+        }
+
+        if (heap[index] >= heap[child_1] && heap[index] >= heap[child_2])
+        {
+            break;
+        }
+
+        if (heap[child_1] > heap[child_2])
+        {
+            swap_child = child_1;
+        }
+        else
+        {
+            swap_child = child_2;
+        }
+
+        swap(heap[index], heap[swap_child]);
+        index = swap_child;
+    }
+
+    return heap;
+}
+
+vector<int> sort(vector<int> numbers)
+{
+    int i, j, size = numbers.size();
+    vector<int> sorted(size);
+
+    numbers = make_heap(numbers);
+
+    for (i = 0; i < size; i++)
+    {
+        sorted[size - i - 1] = numbers[0];
+
+        numbers = remove_top_item(numbers);
+    }
+
+    return sorted;
 }
 
 int main()
 {
+    srand(time(0));
     int i, j, index, len;
     cin >> len;
 
     vector<int> numbers(len);
-    vector<int> sorted(len);
 
     for (i = 0; i < len; i++)
     {
@@ -52,17 +112,11 @@ int main()
     }
     cout << endl << endl;
 
-    numbers = make_heap(numbers);
+    numbers = sort(numbers);
 
-    for (i = 0; i < len; i++)
+    for (j = 0; j < numbers.size(); j++)
     {
-        sorted[len - i - 1] = numbers[0];
-        numbers = remove_top_item(numbers);
-    }
-
-    for (j = 0; j < sorted.size(); j++)
-    {
-        cout << sorted[j] << " ";
+        cout << numbers[j] << " ";
     }
     cout<<endl;
 
